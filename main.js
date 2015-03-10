5dafb5fa72fa6439eef8601ed178a01a670d79fe
Mohamed Ahmed <mohamed.ahmed@live.ca>
2015-03-10 10:44:05 -0400

    removing unnecessary example code

--- main.js
+++ main.js
@@ -48,49 +48,6 @@ var typed = true;
      	globalModel.getRoot().addEventListener(gapi.drive.realtime.EventType.OBJECT_CHANGED, valueChanged);
 
 
-      // Keeping one box updated with a String binder.
-      textArea1 = document.getElementById('editor1');
-      /*gapi.drive.realtime.databinding.bindString(string, textArea1);*/
-
-      // Keeping one box updated with a custom EventListener.
-      var textArea2 = document.getElementById('editor2');
-      var updateTextArea2 = function(e) {
-      	console.log(e);
-      	textArea2.value = string;
-
-      };
-      //string.addEventListener(gapi.drive.realtime.EventType.TEXT_INSERTED, updateTextArea2);
-      //string.addEventListener(gapi.drive.realtime.EventType.TEXT_DELETED, updateTextArea2);
-      textArea2.onkeyup = function() {
-      	string.setText(textArea2.value);
-      };
-      updateTextArea2();
-
-      /*string.addEventListener(gapi.drive.realtime.EventType.VALUE_CHANGED,valueChanged);*/
-      
-
-      // Enabling UI Elements.
-      textArea1.disabled = false;
-      textArea2.disabled = false;
-
-      // Add logic for undo button.
-      var model = doc.getModel();
-      var undoButton = document.getElementById('undoButton');
-      var redoButton = document.getElementById('redoButton');
-
-      undoButton.onclick = function(e) {
-      	model.undo();
-      };
-      redoButton.onclick = function(e) {
-      	model.redo();
-      };
-
-      // Add event handler for UndoRedoStateChanged events.
-      var onUndoRedoStateChanged = function(e) {
-      	undoButton.disabled = !e.canUndo;
-      	redoButton.disabled = !e.canRedo;
-      };
-      model.addEventListener(gapi.drive.realtime.EventType.UNDO_REDO_STATE_CHANGED, onUndoRedoStateChanged);
   }
 
     /**
